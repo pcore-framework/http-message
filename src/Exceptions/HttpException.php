8ca10e08aafd611f2d58a5603048144e44619cd1
Magomedcoder <info@magomedcoder.ru>
2022-07-10 01:21:59 +0300

    Optimize http exception handling

---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\HttpMessage\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class HttpException
+ * @package PCore\HttpMessage\Exceptions
+ * @github https://github.com/pcore-framework/http-message
+ */
+class HttpException extends RuntimeException
+{
+}
